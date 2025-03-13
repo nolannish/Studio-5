097be7731ba52b5d6200a6d3909d26dd3d66e75e
Antonio Vazquez-Mackay <73569624+Tonydvm@users.noreply.github.com>
2025-03-13 15:16:44 -0700

    Implemented camera shake

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
     [SerializeField] private int maxLives = 3;
     [SerializeField] private Ball ball;
     [SerializeField] private Transform bricksContainer;
+    [SerializeField] private float shakeDuration = 0.1f; // How long the camera shake lasts
+    [SerializeField] private float shakeIntensity = 0.2f; // How strong the camera shake is
 
     private int currentBrickCount;
     private int totalBrickCount;
+    private Camera mainCamera;
+    private Vector3 originalCameraPosition;
 
     private void OnEnable()
     {
@@ -15,16 +20,19 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         ball.ResetBall();
         totalBrickCount = bricksContainer.childCount;
         currentBrickCount = bricksContainer.childCount;
+        Debug.Log("[GameManager] OnEnable: Game enabled.");
     }
 
     private void OnDisable()
     {
         InputHandler.Instance.OnFire.RemoveListener(FireBall);
+        Debug.Log("[GameManager] OnDisable: Game disabled.");
     }
 
     private void FireBall()
     {
         ball.FireBall();
+        Debug.Log("[GameManager] FireBall: Ball fired.");
     }
 
     public void OnBrickDestroyed(Vector3 position)
@@ -32,16 +40,55 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         // fire audio here
         // implement particle effect here
         // add camera shake here
+        Debug.Log("[GameManager] OnBrickDestroyed: Starting camera shake.");
+        StartCoroutine(ShakeCamera());
+
         currentBrickCount--;
-        Debug.Log($"Destroyed Brick at {position}, {currentBrickCount}/{totalBrickCount} remaining");
-        if(currentBrickCount == 0) SceneHandler.Instance.LoadNextScene();
+        Debug.Log($"[GameManager] OnBrickDestroyed: Destroyed Brick at {position}, {currentBrickCount}/{totalBrickCount} remaining");
+        if (currentBrickCount == 0) SceneHandler.Instance.LoadNextScene();
     }
 
     public void KillBall()
     {
         maxLives--;
+        Debug.Log($"[GameManager] KillBall: Ball killed, lives remaining: {maxLives}");
         // update lives on HUD here
         // game over UI if maxLives < 0, then exit to main menu after delay
         ball.ResetBall();
+    }
+
+    private IEnumerator ShakeCamera()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+            if (mainCamera == null)
+            {
+                Debug.LogError("No Main Camera found in the scene!");
+                yield break;
+            }
+            originalCameraPosition = mainCamera.transform.localPosition;
+        }
+        else if (originalCameraPosition == Vector3.zero)
+        {
+            originalCameraPosition = mainCamera.transform.localPosition;
+        }
+
+        float elapsed = 0f;
+
+        while (elapsed < shakeDuration)
+        {
+            float x = Random.Range(-1f, 1f) * shakeIntensity;
+            float y = Random.Range(-1f, 1f) * shakeIntensity;
+            Vector3 offset = new Vector3(x, y, 0f);
+
+            mainCamera.transform.localPosition = originalCameraPosition + offset;
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        mainCamera.transform.localPosition = originalCameraPosition;
     }
 }
